7649829bde7603da4873cd14a2de20bcdc26c72c
jrauljperez02 <jrauljperez02.dev@gmail.com>
2023-10-19 18:31:12 -0600

    work: work in progress

--- app/app.py
+++ app/app.py
@@ -1,11 +1,5 @@
 """Script to process the SQS event and insert the data into the database"""
-from db.postgres_connection import PostgresDatabase
-from db.postgres_config import db_config
-from services.sqs_event_processor import SQSEventProcessor
-from queries.insert_data import generate_insert_query
 import json
-
-sqs_event_processor = SQSEventProcessor()
 
 def lambda_handler(event, context):
     """
@@ -15,13 +9,8 @@ def lambda_handler(event, context):
 
         :return: A response object containing the status code and body
     """
-    with PostgresDatabase(config=db_config) as connection:
-        sqs_data = sqs_event_processor.listener(event)
-        query, values = generate_insert_query(sqs_data)
-        cursor = connection.cursor()
-        cursor.execute(query)
-
-        return {
-            'statusCode': 200,
-            'body': json.dumps('Mensaje procesado'),
-        }
+    print('Event: ', event)
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Mensaje procesado'),
+    }
