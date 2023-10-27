ef861e20e8eb2156e1d27779a98ea0c6b81973a3
jrauljperez02 <jrauljperez02.dev@gmail.com>
2023-10-27 00:19:54 -0600

    feat: include db configuration

--- app/app.py
+++ app/app.py
@@ -1,5 +1,7 @@
 """Script to process the SQS event and insert the data into the database"""
 import json
+from db.postgres_config import db_config
+from db.postgres_connection import PostgresDatabase
 
 def handler(event, context):
     """
@@ -9,8 +11,12 @@ def handler(event, context):
 
         :return: A response object containing the status code and body
     """
-    print(event)
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Mensaje procesado')
-    }
+    with PostgresDatabase(db_config) as connection:
+        cursor = connection.cursor()
+        data = cursor.execute("SELECT * FROM core_user;")
+        print('Data from database: ', data)
+    
+        return {
+            'statusCode': 200,
+            'body': json.dumps('Mensaje procesado')
+        }
