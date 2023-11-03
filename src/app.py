ec8fb119ace58418f7237d8b59e4850af7103beb
jrauljperez02 <jrauljperez02.dev@gmail.com>
2023-11-03 17:13:56 -0600

    feat: include db connection

--- src/app.py
+++ src/app.py
@@ -1,6 +1,8 @@
 """Script to process the SQS event and insert the data into the database"""
 import json
 from services.sqs_event_processor import SQSEventProcessor
+from db.postgres_config import db_config
+from db.postgres_connection import PostgresDatabase
 import psycopg2
 
 def handler(event, context):
@@ -12,11 +14,18 @@ def handler(event, context):
         :return: A response object containing the status code and body
     """
     sqs_event_processor = SQSEventProcessor(event)
-    processed_data = sqs_event_processor.process_message()
-
-    print('Processed data: {}'.format(processed_data))
     
+
+    with PostgresDatabase(db_config) as connection:
+        cursor = connection.cursor()
+        data = cursor.execute("SELECT * FROM core_user;")
+        processed_data = sqs_event_processor.process_message()
+
+        print('Data from database: ', data)
+        print('Processed data: ', processed_data)
+
+
     return {
         'statusCode': 200,
-        'body': json.dumps('AWS Lambda function executed successfully!')
+        'body': json.dumps('AWS Lambda SQS event processed successfully!')
     }
