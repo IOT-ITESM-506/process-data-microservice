08355b4eab36d68e56cb219741b65ce9781ae400
jrauljperez02 <jrauljperez02.dev@gmail.com>
2023-11-02 18:48:13 -0600

    feat: include payload body

--- app/app.py
+++ app/app.py
@@ -1,10 +1,8 @@
 """Script to process the SQS event and insert the data into the database"""
 import json
-# from db.postgres_config import db_config
-# from db.postgres_connection import PostgresDatabase
-import psycopg2
+from services.sqs_event_processor import SQSEventProcessor
 
-def lambda_handler(event, context):
+def handler(event, context):
     """
         Handler function for the Lambda function
         :param event: The event data passed to the Lambda function
@@ -12,12 +10,10 @@ def lambda_handler(event, context):
 
         :return: A response object containing the status code and body
     """
-    print('Event: ', event)
-    # print("JESUS RAUL Psycopg2 Version:", psycopg2.__version__)
-    # with PostgresDatabase(db_config) as connection:
-    #     cursor = connection.cursor()
-    #     data = cursor.execute("SELECT * FROM core_user;")
-    #     print('Data from database: ', data)
+    sqs_event_processor = SQSEventProcessor(event)
+    processed_data = sqs_event_processor.process_message()
+
+    print('Processed data: {}'.format(processed_data))
     
     return {
         'statusCode': 200,
--- app/services/sqs_event_processor.py
+++ app/services/sqs_event_processor.py
@@ -1,32 +1,46 @@
-"""SQS event processor"""
-import boto3
 import json
 
 class SQSEventProcessor:
-    def __init__(self):
-        self.sqs = boto3.client('sqs')
+    def __init__(self, event):
+        self.event = event
 
-    def listener(self, event):
-        """AWS Lambda function handler"""
-        for record in event['Records']:
-            body = json.loads(record['body'])
-            self.process_message(body)
-
-    def process_message(self, message):
-        """Process a message from the SQS queue"""
-
-        return {
-            'statusCode': 200,
-            'body': json.dumps('Mensaje procesado'),
-            'payload': {
-                'greenhouse_id': message['greenhouse_id'],
-                'temperature': message['temperature'],
-                'humidity': message['humidity'],
-                'luminosity': message['luminosity'],
-                'CO2_level': message['CO2_level'],
-                'soil_moisture': message['soil_moisture'],
-                'pH': message['pH'],
-                'nutrient_level': message['nutrient_level'],
-                'timestamp': message['timestamp']
-            }
+    def process_message(self):
+        processed_data = {
+            'temperature': None,
+            'humidity': None,
+            'luminosity': None,
+            'co2_level': None,
+            'soil_moisture': None,
+            'ph': None,
+            'nutrient_level': None,
+            'timestamp': None,
+            'sensor_record_circuit_id': None,
         }
+
+        records = self.event.get('Records', [])
+
+        if records:
+            record = records[0]
+            message_body = record.get('body', None)
+
+            if message_body:
+                try:
+                    sns_message = json.loads(message_body)                    
+                    message_data = json.loads(sns_message.get('Message', '{}'))
+                    processed_data.update({
+                        'temperature': message_data.get('temperature', None),
+                        'humidity': message_data.get('humidity', None),
+                        'luminosity': message_data.get('luminosity', None),
+                        'co2_level': message_data.get('co2_level', None),
+                        'soil_moisture': message_data.get('soil_moisture', None),
+                        'ph': message_data.get('ph', None),
+                        'nutrient_level': message_data.get('nutrient_level', None),
+                        'timestamp': message_data.get('timestamp', None),
+                        'sensor_record_circuit_id': message_data.get('sensor_record_circuit_id', None),
+                    })
+                except json.JSONDecodeError as e:
+                    print(f"Error decoding JSON: {e}")
+            else:
+                print("No 'body' field in the record.")
+
+        return processed_data
