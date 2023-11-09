5758417dc539ffeb4725851d4c12225b15e09861
jrauljperez02 <jrauljperez02.dev@gmail.com>
2023-11-09 17:58:25 -0600

    work: work in progress

--- src/queries/insert_record_data.py
+++ src/queries/insert_record_data.py
@@ -4,15 +4,6 @@ def insert_record_data(data):
             INSERT INTO core_sensorrecord (id, temperature, humidity, luminosity, timestamp, greenhouse_id)
             VALUES (%s, %s, %s, %s, %s, %s);
         """
-
-        values = (
-            data['id'],
-            data['temperature'],
-            data['humidity'],
-            data['luminosity'],
-            data['timestamp'],
-            data['sensor_record_circuit_id']
-        )
-        return query, values
+        return query
     except Exception as e:
         return None, f"Error inserting record data: {e}"
